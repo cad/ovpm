333665db6050d962befebeeb3574cee072b05864
Mustafa Arici <mustafa@arici.io>
2017-08-08 09:26:02 +0300

    commit 8360e6265d3b4ad6f1cbf0f176a47aaf40322ff0 Author: Mustafa Arici <mustafa@arici.io> Date:   Wed Jul 26 05:43:51 2017 +0300
        feat: remote control protocol
    
        In order to seperate cli with the core of the system we had to come up
        with a remote control system. gRPC is used because of its performance
        and generic approach.

--- db.go
+++ db.go
@@ -3,60 +3,12 @@ package ovpm
 import (
 	"github.com/Sirupsen/logrus"
 	"github.com/jinzhu/gorm"
+
+	// We blank import sqlite here because gorm needs it.
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var db *gorm.DB
-
-// User is database model for VPN users.
-type User struct {
-	gorm.Model
-	ServerID           uint
-	Server             Server
-	ServerSerialNumber string
-
-	Username string `gorm:"unique_index"`
-	Password string
-	Cert     string
-	Key      string
-}
-
-func (u *User) setPassword(newPassword string) error {
-	// TODO(cad): Use a proper password hashing algorithm here.
-	u.Password = newPassword
-	return nil
-}
-
-// Network is database model for external networks on the VPN server.
-type Network struct {
-	gorm.Model
-	ServerID uint
-	Server   Server
-
-	Name        string
-	NetworkCIDR string
-}
-
-// Server is database model for storing VPN server related stuff.
-type Server struct {
-	gorm.Model
-	Name         string `gorm:"unique_index"` // Server name.
-	SerialNumber string
-
-	Hostname string // Server's ip address or FQDN
-	Port     string // Server's listening port
-	Cert     string // Server RSA certificate.
-	Key      string // Server RSA private key.
-	CACert   string // Root CA RSA certificate.
-	CAKey    string // Root CA RSA key.
-	Net      string // VPN network.
-	Mask     string // VPN network mask.
-}
-
-// CheckSerial takes a serial number and checks it against the current server's serial number.
-func (s *Server) CheckSerial(serialNo string) bool {
-	return serialNo == s.SerialNumber
-}
 
 // CloseDB closes the database.
 func CloseDB() {
@@ -70,7 +22,9 @@ func init() {
 		logrus.Fatalf("couldn't open sqlite database %s: %v", DefaultDBPath, err)
 	}
 
-	db.AutoMigrate(&User{})
-	db.AutoMigrate(&Network{})
-	db.AutoMigrate(&Server{})
+	db.AutoMigrate(&DBUser{})
+	db.AutoMigrate(&DBNetwork{})
+	db.AutoMigrate(&DBServer{})
+	db.AutoMigrate(&DBRevoked{})
+
 }
