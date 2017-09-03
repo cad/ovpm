35f55b838c8d8b9c2686b749e7a4656323e24d08
Mustafa Arici <mustafa@arici.io>
2017-09-03 17:08:33 +0300

    release: v0.1.14

--- const.go
+++ const.go
@@ -2,7 +2,7 @@ package ovpm
 
 const (
 	// Version defines the version of ovpm.
-	Version = "0.1.13"
+	Version = "0.1.14"
 
 	// DefaultVPNPort is the default OpenVPN port to listen.
 	DefaultVPNPort = "1197"
