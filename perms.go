9c34a5fcb16b9644dbf2ba4bde0991960d50150f
Mustafa Arici <mustafa.arici@neu.edu.tr>
2017-10-02 17:21:07 +0300

    feat(permset): apply permission checking using permsets

--- perms.go
+++ perms.go
@@ -2,12 +2,64 @@ package ovpm
 
 import "github.com/cad/ovpm/permset"
 
-// OVPM defined permissions.
+// OVPM available permissions.
 const (
+	// User permissions
 	CreateUserPerm permset.Perm = iota
 	GetAnyUserPerm
 	GetSelfPerm
 	UpdateAnyUserPerm
 	UpdateSelfPerm
 	DeleteAnyUserPerm
+	RenewAnyUserPerm
+	GenConfigAnyUserPerm
+	GenConfigSelfPerm
+
+	// VPN permissions
+	GetVPNStatusPerm
+	InitVPNPerm
+	UpdateVPNPerm
+
+	// Network permissions
+	ListNetworksPerm
+	CreateNetworkPerm
+	DeleteNetworkPerm
+	GetNetworkTypesPerm
+	GetNetworkAssociatedUsersPerm
+	AssociateNetworkUserPerm
+	DissociateNetworkUserPerm
 )
+
+// AdminPerms is a collection of permissions for Admin.
+func AdminPerms() []permset.Perm {
+	return []permset.Perm{
+		CreateUserPerm,
+		GetAnyUserPerm,
+		GetSelfPerm,
+		UpdateAnyUserPerm,
+		UpdateSelfPerm,
+		DeleteAnyUserPerm,
+		RenewAnyUserPerm,
+		GenConfigAnyUserPerm,
+		GenConfigSelfPerm,
+		GetVPNStatusPerm,
+		InitVPNPerm,
+		UpdateVPNPerm,
+		ListNetworksPerm,
+		CreateNetworkPerm,
+		DeleteAnyUserPerm,
+		GetNetworkTypesPerm,
+		GetNetworkAssociatedUsersPerm,
+		AssociateNetworkUserPerm,
+		DissociateNetworkUserPerm,
+	}
+}
+
+// UserPerms is a collection of permissions for User.
+func UserPerms() []permset.Perm {
+	return []permset.Perm{
+		GetSelfPerm,
+		UpdateSelfPerm,
+		GenConfigSelfPerm,
+	}
+}
