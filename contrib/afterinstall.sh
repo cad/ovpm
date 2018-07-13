73798c4d80dbbf3b87eff2f889e589ab03784b24
Mustafa Arici <mustafa@arici.io>
2018-07-13 11:25:43 +0300

    chore(build): fix typo

--- contrib/afterinstall.sh
+++ contrib/afterinstall.sh
@@ -2,6 +2,6 @@
 export USER="nobody"
 export GROUP="nogroup"
 id -u $USER &>/dev/null || useradd $USER
-id -g $GROUP &>/dev/null || groudadd $GROUP
+id -g $GROUP &>/dev/null || groupadd $GROUP
 
 systemctl daemon-reload
