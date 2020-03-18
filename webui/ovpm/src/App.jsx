617f786cc71a2a0b3deeb74e544d078dfb96d769
drac <seyhan.drc@gmail.com>
2020-03-18 20:00:00 +0200

    BREAKING CHANGE: refactor project runner using react-scripts
    - implement react-scripts
    - implement yarn
    - update README.md
    - upgrade npm packages
    - use .css instead of .less

---
+++ webui/ovpm/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Master from "./components/Master";
+
+function App() {
+  return (
+    <div>
+      <Master />
+    </div>
+  );
+}
+
+export default App;
---
+++ webui/ovpm/src/components/Master/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import style from "./style.css";
+
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+
+import Login from "../Auth/Login";
+import Logout from "../Auth/Logout";
+import LoginRequired from "../Auth/LoginRequired";
+
+import Dashboard from "../Dashboard";
+
+function Home(props) {
+  return <Redirect to="/dashboard" />;
+}
+
+const Master = props => (
+  <Router>
+    <div>
+      <Route path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/logout" component={Logout} />
+      <Route path="/dashboard" component={LoginRequired(Dashboard)} />
+    </div>
+  </Router>
+);
+export default Master;
